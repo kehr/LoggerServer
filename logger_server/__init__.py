2f6efda770f413e54497808e615598f7e3ca67ca
wangkaixuan <kehr.china@gmail.com>
2017-11-20 16:23:48 +0800

    remove LoggerStreamHandler

--- logger_server/__init__.py
+++ logger_server/__init__.py
@@ -5,12 +5,11 @@
 @Filename: __init__.py
 @Author: Kehr <kehr.china@gmail.com>
 @Created Date:   2017-11-15T19:16:34+08:00
-@Last modified time: 2017-11-16T12:59:59+08:00
+@Last modified time: 2017-11-20T16:23:36+08:00
 @License: Apache License <http://www.apache.org/licenses/>
 """
 from server import AttrDict
 from server import LoggerFormatter
-from server import LoggerStreamHandler
 from server import LoggerServer
 from server import version
 from server import __version__
